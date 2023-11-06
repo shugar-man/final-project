1878f6242549e5987fdd1b37b70ed3886202c248
carry212121 <harris.m@ku.th>
2023-11-06 21:10:23 +0700

    css admin

--- frontend-project/pages/admin/index.vue
+++ frontend-project/pages/admin/index.vue
@@ -1,28 +1,36 @@
 <template>
-  <div>
+  <div style="padding-inline-start: 40px;margin-block-end: 1em;margin-block-start: 1em;list-style-type: disc;background-color: #042f55; background-image: url(/images/bg.gif); background-repeat: repeat-x; display: block;">
     <h2 class="text-2xl font-semibold mb-4">Reported Posts</h2>
-    <ul>
-      <li v-for="post in reportedPosts" :key="post.id" class="mb-4 p-4 bg-white rounded shadow-md">
-        <div class="flex items-center mb-2">
-          <nuxt-link :to="`/page/${post.user_id}`" class="flex items-center">
-            <img :src="imageURL(post.user_profile_image)" class="rounded-full h-12 w-12 mr-2" />
-            <span class="text-lg font-semibold">{{ post.user_name }}</span>
-          </nuxt-link>
+    <div class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10">
+      <div v-for="post in reportedPosts" :key="post.id" class="mb-4 p-4 bg-white rounded shadow-md">
+        <div class="hover:bg-gray-900 hover:text-white transition duration-300 max-w-sm rounded overflow-hidden shadow-lg">
+          <div class="py-4 px-8">
+        
+            <div class="" style="width: 1369px; contain-intrinsic-size: 1369px 282.955px; content-visibility: auto;">
+              <div class="flex items-center mb-2">
+                <nuxt-link :to="`/page/${post.user_id}`" class="flex items-center">
+                  <img :src="imageURL(post.user_profile_image)" class="rounded-full h-12 w-12 mr-2" />
+                  <span class="text-lg font-semibold">{{ post.user_name }}</span>
+                </nuxt-link>
+              </div>
+              <div>
+                <nuxt-link :to="`/posts/${post.id}`" class="flex items-center">
+                  <img :src="imageURL(post.post_image_path)" class="w-24 h-24 object-cover mr-2" />
+                  <span class="text-lg font-semibold">{{ post.post_name }}</span>
+                </nuxt-link>
+              </div>
+              <button @click="deletePost(post)" class="mt-2 bg-red-500 text-white rounded px-4 py-2 hover:bg-red-700">
+                Delete
+              </button>
+            
+            <div v-if="reportedPosts.length === 0" class="text-lg text-gray-600 mt-4">
+              No reported posts
+            </div>
+          </div>
         </div>
-        <div>
-          <nuxt-link :to="`/posts/${post.id}`" class="flex items-center">
-            <img :src="imageURL(post.post_image_path)" class="w-24 h-24 object-cover mr-2" />
-            <span class="text-lg font-semibold">{{ post.post_name }}</span>
-          </nuxt-link>
-        </div>
-        <button @click="deletePost(post)" class="mt-2 bg-red-500 text-white rounded px-4 py-2 hover:bg-red-700">
-          Delete
-        </button>
-      </li>
-      <div v-if="reportedPosts.length === 0" class="text-lg text-gray-600 mt-4">
-        No reported posts
       </div>
-    </ul>
+    </div>
+  </div>
   </div>
 </template>
 
